241a417168550c4cd8e875c7ba1a245d21f59b25
Sowrov Sharif <61372968+sowrovsharif@users.noreply.github.com>
2023-01-14 22:43:28 +0600

    Update Understanding Bitmask.cpp

--- Bitmask Problem/Understanding Bitmask.cpp
+++ Bitmask Problem/Understanding Bitmask.cpp
@@ -5,6 +5,7 @@ char comp[3]={'A','B','C'};
 int main() 
 {
   //Creating Bitmask and Print Combination
+  //8 Or 1<<N is Equal to 2^n Here n is 3
     for(int i=0;i<8;i++){
       for(int j=0;j<3;j++){
         if((i&(1<<j))){
